42aea38d5cdcc6345c9179b3a5c952217224fd84
Arka Karmoker <91338507+ArkaKarmoker@users.noreply.github.com>
2023-01-08 16:54:40 +0600

    Update main.py

--- Blind Auction/main.py
+++ Blind Auction/main.py
@@ -1,4 +1,3 @@
-# from replit import clear
 from art import logo
 print(logo)
 
@@ -27,13 +26,3 @@ while not bidding_finished:
   elif should_continue == "yes":
     continue
   
-
-"""
-FAQ: My console doesn't clear()
-
-This will happen if you’re using an IDE other than replit. 
-I’ll cover how to use PyCharm in Day 15. That said, you can write your own clear() function or configure your IDE like so: 
-
-https://www.udemy.com/course/100-days-of-code/learn/lecture/19279420#questions/16084076
-
-"""
